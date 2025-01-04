c6d2ba9b602453c2e47e48369dc0dba4a23c5f73
Dominik Brzeziński <scnstr@proton.me>
2025-01-04 01:06:43 +0100

    fen parsing, bitboards, pawn and king moves

--- chess.sh
+++ chess.sh
@@ -2,55 +2,102 @@
 
 stty cbreak -echo -nl
 
-board=( \
-r b n q k n b r \
-p p p p p p p p \
-. . . . . . . . \
-. . . . . . . . \
-. . . . . . . . \
-. . . . . . . . \
-P P P P P P P P \
-R B N Q K N B R \
-)
+board=()
+active=
 
-row=8
-col=8
+parse_fen() {
+    fen_string="$1"
+    IFS=' ' read -r fen_board active castling en_passant halfmove fullmove <<< "$fen_string"
 
-for piece in ${board[@]}; do
-    fg_col="\e[30m" # black
-    bg_col="\e[46m" # cyan
-    if (((row + col) % 2 == 0)); then
-        bg_col="\e[107m" #bright white
-    fi
+    j=0
+    for ((i=0; i<${#fen_board}; i++)); do
+        char="${fen_board:$i:1}"
+        
+        case $char in
+            [a-zA-Z]) board[j]=$char; ((j++));;
+            [1-8]) 
+                for ((k=0; k<$char; k++)) {
+                    board[j]="."
+                    ((j++))
+                }
+                ;;
 
-    case $piece in
-        [K]) piece=♔ ;;
-        [Q]) piece=♕ ;;
-        [R]) piece=♖ ;;
-        [B]) piece=♗ ;;
-        [N]) piece=♘ ;;
-        [P]) piece=♙ ;;
-        [k]) piece=♚ ;;
-        [q]) piece=♛ ;;
-        [r]) piece=♜ ;;
-        [b]) piece=♝ ;;
-        [n]) piece=♞ ;;
-        [p]) piece=♟ ;;
-        ".") piece=" ";;
-    esac
+        esac
+    done
+}
 
-    printf "$bg_col$fg_col$piece "
-    if ((--col < 1 )); then
-        col=8
-        printf "\e[m $row\n"
-        ((row--))
-    fi
-done
+compose_fen() {
+    fen_board=""
+    empty_count=0
+    file_count=0
+    for piece in ${board[@]}; do
+        if (( $file_count >= 8)); then
+            if (( $empty_count > 0)); then
+                fen_board+="$empty_count"
+                empty_count=0
+            fi
+            file_count=0
+            fen_board+="/"
+        fi
 
-printf "A B C D E F G H\n"
+        if [[ $piece = "." ]]; then
+            ((empty_count++))
+        else
+            if (( $empty_count > 0)); then
+                fen_board+="$empty_count"
+                empty_count=0
+            fi
+            fen_board+=$piece
+        fi
+        ((file_count++))
+    done
+
+    echo "$fen_board"
+}
+
+draw() {
+    row=8
+    col=8
+    for piece in ${board[@]}; do
+        fg_col="\e[30m" # black
+        bg_col="\e[46m" # cyan
+        if (((row + col) % 2 == 0)); then
+            bg_col="\e[107m" #bright white
+        fi
+
+        case $piece in
+            [K]) piece=♔ ;;
+            [Q]) piece=♕ ;;
+            [R]) piece=♖ ;;
+            [B]) piece=♗ ;;
+            [N]) piece=♘ ;;
+            [P]) piece=♙ ;;
+            [k]) piece=♚ ;;
+            [q]) piece=♛ ;;
+            [r]) piece=♜ ;;
+            [b]) piece=♝ ;;
+            [n]) piece=♞ ;;
+            [p]) piece=♟ ;;
+            ".") piece=" ";;
+        esac
+
+        printf "$bg_col$fg_col$piece "
+        if ((--col < 1 )); then
+            col=8
+            printf "\e[m $row\n"
+            ((row--))
+        fi
+    done
+
+    printf "A B C D E F G H\n"
+}
+
+parse_fen "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+# parse_fen "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"
+draw
+compose_fen
 
 move=
-
 
 while true; do
     char=$(dd bs=1 count=1 2>/dev/null)
--- chess_engine.pl
+++ chess_engine.pl
@@ -2,66 +2,234 @@
 use strict;
 use warnings;
 
-if (scalar(@ARGV) == 0) {
-    printf "invalid\n";
+use constant {
+        RANK_1 => 0xFF << (8 * 0),
+        RANK_2 => 0xFF << (8 * 1),
+        RANK_3 => 0xFF << (8 * 2),
+        RANK_4 => 0xFF << (8 * 3),
+        RANK_5 => 0xFF << (8 * 4),
+        RANK_6 => 0xFF << (8 * 5),
+        RANK_7 => 0xFF << (8 * 6),
+        RANK_8 => 0xFF << (8 * 7),
+        FILE_A => 0x101010101010101,
+        FILE_B => 0x101010101010101 << 1,
+        FILE_C => 0x101010101010101 << 2,
+        FILE_D => 0x101010101010101 << 3,
+        FILE_E => 0x101010101010101 << 4,
+        FILE_F => 0x101010101010101 << 5,
+        FILE_G => 0x101010101010101 << 6,
+        FILE_H => 0x101010101010101 << 7,
+};
+
+my $board = {};
+
+sub parse_fen {
+    my ($fen) = @_;
+    my ($fen_board, $turn, $castling, $en_passant, $halfmove, $fullmove) = split / /, $fen;
+
+    $board = {
+        turn => $turn eq 'w' ? 0 : 1,
+        halfmove => $halfmove,
+        fullmove => $fullmove,
+        en_passant => $en_passant eq "-" ? -1 : notation_to_index($en_passant),
+        castling => -1,
+        pieces => {
+            P => 0,
+            R => 0,
+            B => 0,
+            N => 0,
+            Q => 0,
+            K => 0,
+            p => 0,
+            r => 0,
+            b => 0,
+            n => 0,
+            q => 0,
+            k => 0,
+        }
+    };
+
+    my $mask = 1 << 63;
+    foreach my $char (split //, $fen_board) {
+        if ($char =~ /[rbnqkp]/i) {
+            $board->{pieces}->{$char} |= $mask;
+            $mask >>= 1;
+        } elsif ($char =~ /[1-8]/) {
+            $mask >>= $char;
+        }
+    }
 }
 
-my $move = $ARGV[0];
+sub white_pieces {
+    return $board->{pieces}->{P} | $board->{pieces}->{R} | $board->{pieces}->{B}
+        | $board->{pieces}->{N} | $board->{pieces}->{Q} | $board->{pieces}->{K};
+}
 
-my @board = (
-"R", "B", "N", "Q", "K", "N", "B", "R",
-"P", "P", "P", "P", "P", "P", "P", "P",
-".", ".", ".", ".", ".", ".", ".", ".",
-".", ".", ".", ".", ".", ".", ".", ".",
-".", ".", ".", ".", ".", ".", ".", ".",
-".", ".", ".", ".", ".", ".", ".", ".",
-"p", "p", "p", "p", "p", "p", "p", "p",
-"r", "b", "n", "q", "k", "n", "b", "r",
-);
+sub black_pieces {
+    return $board->{pieces}->{p} | $board->{pieces}->{r} | $board->{pieces}->{b}
+        | $board->{pieces}->{n} | $board->{pieces}->{q} | $board->{pieces}->{k};
+}
 
-if (scalar(@ARGV) > 1) {
-    @board = split(" ", $ARGV[1]);
+sub all_pieces {
+    return white_pieces() | black_pieces();
 }
 
 sub notation_to_index {
     my $square = shift;
-    my $col = ord(substr($square, 0, 1)) - ord("a");
+    my $col = ord("h") - ord(substr($square, 0, 1));
     my $row = substr($square, 1, 1) - 1;
     return $row * 8 + $col;
 }
 
-sub parse_move {
-    if ($move !~ /[a-h][1-8][a-h][1-8]/i) {
-        print "Invalid move\n";
-        exit 1;
-    }
-    my $from = substr($move, 0, 2);
-    my $to = substr($move, 2, 2);
-    return (notation_to_index($from), notation_to_index($to));
+sub index_to_notation {
+    my $square = shift;
+    my $col = $square % 8;
+    my $row = int($square / 8) + 1;
+    return chr(ord("h") - $col) . $row;
 }
 
-sub generate_w_pawn_moves {
+#sub parse_move {
+#    my $move = 
+#    if ($move !~ /[a-h][1-8][a-h][1-8]/i) {
+#        print "Invalid move\n";
+#        exit 1;
+#    }
+#    my $from = substr($move, 0, 2);
+#    my $to = substr($move, 2, 2);
+#    return (notation_to_index($from), notation_to_index($to));
+#}
 
+sub move_to_string {
+    my $move = shift;
+    my $from = ($move >> 4) & 63;
+    my $to = ($move >> 12) & 63;
+    my $promotion = $move & 15;
+    my $promotion_string = "";
+    if ($promotion eq 1) {
+        $promotion_string = "=r"
+    } elsif ($promotion eq 2) {
+        $promotion_string = "=n"
+    } elsif ($promotion eq 3) {
+        $promotion_string = "=b"
+    } elsif ($promotion eq 4) {
+        $promotion_string = "=q"
+    }
+    return index_to_notation($from) . index_to_notation($to) . $promotion_string;
 }
 
-sub is_legal {
-    my ($from, $to) = @_;
-    if ($board[$from] eq ".") {
-        return 0;
+sub pawn_moves {
+    my $bit_moves;
+    my @moves;
+
+    if (!$board->{turn}) {
+        $bit_moves = ($board->{pieces}->{P} << 8) & (~all_pieces());
+        for my $i (0..63) {
+            my $mask = 1 << $i;
+            if ($bit_moves & $mask) {
+                if ($i >= 56) {
+                    for my $j (1..4) {
+                        push(@moves, $j | (($i - 8) << 4) | ($i << 12));
+                    }
+                } else {
+                    push(@moves, 0 | (($i - 8) << 4) | ($i << 12));
+                }
+            }
+        }
+    } else {
+        $bit_moves = ($board->{pieces}->{p} >> 8) & (~all_pieces());
+        for my $i (0..63) {
+            my $mask = 1 << $i;
+            if ($bit_moves & $mask) {
+                if ($i < 8) {
+                    for my $j (1..4) {
+                        push(@moves, $j | (($i + 8) << 4) | ($i << 12));
+                    }
+                } else {
+                    push(@moves, 0 | (($i + 8) << 4) | ($i << 12));
+                }
+            }
+        }
     }
 
-    my $piece = $board[$from];
-
-    if (($piece eq "P" && $to != $from + 8) || ($piece eq "p" && $to != $from - 8)) {
-        return 0;
+    if (!$board->{turn}) {
+        $bit_moves = ($board->{pieces}->{P} & RANK_2) << 16;
+        $bit_moves &= ~all_pieces();
+        $bit_moves &= ~(all_pieces() & RANK_3) << 8;
+        for my $i (0..63) {
+            my $mask = 1 << $i;
+            if ($bit_moves & $mask) {
+                push(@moves, 0 | (($i - 16) << 4) | ($i << 12));
+            }
+        }
+    } else {
+        $bit_moves = ($board->{pieces}->{p} & RANK_7) >> 16;
+        $bit_moves &= ~all_pieces();
+        $bit_moves &= ~(all_pieces() & RANK_6) >> 8;
+        for my $i (0..63) {
+            my $mask = 1 << $i;
+            if ($bit_moves & $mask) {
+                push(@moves, 0 | (($i + 16) << 4) | ($i << 12));
+            }
+        }
     }
-    if ($piece eq )
-
-    return 1;
+    return @moves;
 }
 
-my @parsed_move = parse_move();
-if (is_legal(@parsed_move) == 0) {
-    print "Illegal move\n";
-    exit 1
+sub king_moves {
+    my $bit_moves;
+    my @moves;
+
+    my $king = $board->{turn} ? $board->{pieces}->{k} : $board->{pieces}->{K};
+    $bit_moves = ($king & ~RANK_1) >> 8;
+    $bit_moves |= ($king & ~RANK_8) << 8;
+    $bit_moves |= ($king & ~FILE_A) << 1;
+    $bit_moves |= ($king & ~FILE_H) >> 1;
+    $bit_moves |= ($king & ~(RANK_1 | FILE_A)) >> 7;
+    $bit_moves |= ($king & ~(RANK_1 | FILE_H)) >> 9;
+    $bit_moves |= ($king & ~(RANK_8 | FILE_A)) << 9;
+    $bit_moves |= ($king & ~(RANK_8 | FILE_H)) << 7;
+
+    $bit_moves &= $board->{turn} ? ~black_pieces() : ~white_pieces();
+
+    my $from;
+
+    for my $i (0..63) {
+        my $mask = 1 << $i;
+        if ($king & $mask) {
+            $from = $i;
+            last;
+        }
+    }
+
+    for my $i (0..63) {
+        my $mask = 1 << $i;
+        if ($bit_moves & $mask) {
+            push(@moves, 0 | ($from << 4) | ($i << 12));
+        }
+    }
+
+    return @moves;
+}
+
+sub moves {
+    return (pawn_moves(), king_moves());
+}
+
+sub print_board {
+    my $board = shift;
+    for my $i (0..63) {
+        print $board >> (63 - $i) & 1;
+        if ($i % 8 == 7) {
+            print "\n";
+        }
+    }
+    print "\n";
+}
+
+parse_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+
+my @moves = moves();
+print scalar @moves, "\n";
+foreach my $move (@moves) {
+    print move_to_string($move), "\n";
 }
